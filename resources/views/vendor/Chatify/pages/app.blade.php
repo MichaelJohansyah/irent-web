ae943be258b2a47371f628084d751efc8ed7a6da
Muhammad Azka Ibrahim <aazzkkaa14@gmail.com>
2025-06-27 16:30:49 +0700

    chat message

--- resources/views/vendor/Chatify/pages/app.blade.php
+++ resources/views/vendor/Chatify/pages/app.blade.php
@@ -8,38 +8,33 @@
                 <a href="#"><i class="fas fa-inbox"></i> <span class="messenger-headTitle">MESSAGES</span> </a>
                 {{-- header buttons --}}
                 <nav class="m-header-right">
-                    <a href="#"><i class="fas fa-cog settings-btn"></i></a>
                     <a href="#" class="listView-x"><i class="fas fa-times"></i></a>
                 </nav>
             </nav>
-            {{-- Search input --}}
-            <input type="text" class="messenger-search" placeholder="Search" />
-            {{-- Tabs --}}
-            {{-- <div class="messenger-listView-tabs">
+            {{-- Hidden Search input (required by JavaScript) --}}
+            <input type="text" class="messenger-search" placeholder="Search" style="display: none;" />
+            {{-- Hidden Tabs (required by JavaScript) --}}
+            <div class="messenger-listView-tabs" style="display: none;">
                 <a href="#" class="active-tab" data-view="users">
                     <span class="far fa-user"></span> Contacts</a>
-            </div> --}}
+            </div>
         </div>
         {{-- tabs and lists --}}
         <div class="m-body contacts-container">
            {{-- Lists [Users/Group] --}}
            {{-- ---------------- [ User Tab ] ---------------- --}}
            <div class="show messenger-tab users-tab app-scroll" data-view="users">
-               {{-- Favorites --}}
-               <div class="favorites-section">
+               {{-- Hidden Favorites (required by JavaScript) --}}
+               <div class="favorites-section" style="display: none !important; visibility: hidden !important; height: 0 !important; overflow: hidden !important; opacity: 0 !important;">
                 <p class="messenger-title"><span>Favorites</span></p>
                 <div class="messenger-favorites app-scroll-hidden"></div>
                </div>
-               {{-- Saved Messages --}}
-               <p class="messenger-title"><span>Your Space</span></p>
-               {!! view('Chatify::layouts.listItem', ['get' => 'saved']) !!}
                {{-- Contact --}}
                <p class="messenger-title"><span>All Messages</span></p>
-               <div class="listOfContacts" style="width: 100%;height: calc(100% - 272px);position: relative;"></div>
+               <div class="listOfContacts" style="width: 100%;height: calc(100% - 80px);position: relative;"></div>
            </div>
-             {{-- ---------------- [ Search Tab ] ---------------- --}}
-           <div class="messenger-tab search-tab app-scroll" data-view="search">
-                {{-- items --}}
+             {{-- Hidden Search Tab (required by JavaScript) --}}
+           <div class="messenger-tab search-tab app-scroll" data-view="search" style="display: none;">
                 <p class="messenger-title"><span>Search</span></p>
                 <div class="search-records">
                     <p class="message-hint center-el"><span>Type to search..</span></p>
@@ -62,9 +57,7 @@
                 </div>
                 {{-- header buttons --}}
                 <nav class="m-header-right">
-                    <a href="#" class="add-to-favorite"><i class="fas fa-star"></i></a>
-                    <a href="/"><i class="fas fa-home"></i></a>
-                    <a href="#" class="show-infoSide"><i class="fas fa-info-circle"></i></a>
+                    <a href="#" class="show-infoSide" style="display: none;"><i class="fas fa-info-circle"></i></a>
                 </nav>
             </nav>
             {{-- Internet connection --}}
@@ -97,8 +90,8 @@
         {{-- Send Message Form --}}
         @include('Chatify::layouts.sendForm')
     </div>
-    {{-- ---------------------- Info side ---------------------- --}}
-    <div class="messenger-infoView app-scroll">
+    {{-- ---------------------- Hidden Info side (required by JavaScript) ---------------------- --}}
+    <div class="messenger-infoView app-scroll" style="display: none;">
         {{-- nav actions --}}
         <nav>
             <p>User Details</p>
