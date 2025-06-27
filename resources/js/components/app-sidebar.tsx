9da3d8d06b2fa8ef059fb7fe66a701cfe0e48f45
HafizD <hafizdicky111@gmail.com>
2025-06-27 12:23:36 +0700

    Update app-sidebar.tsx

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -2,7 +2,7 @@ import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { Link, usePage } from '@inertiajs/react';
-import { Bell, LayoutGrid, ListOrdered, MessageCircle } from 'lucide-react';
+import { Bell, FileText, LayoutGrid, ListOrdered, MessageCircle, Plus, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 export function AppSidebar() {
@@ -13,11 +13,11 @@ export function AppSidebar() {
     const isPartner = userRole === 'partner';
     const isAdmin = userRole === 'admin';
     const partnerNavItems = [
-        { title: 'Add Product', href: '/dashboard/add-product', icon: MessageCircle },
+        { title: 'Add Product', href: '/dashboard/add-product', icon: Plus },
         { title: 'Check Notification', href: '/notifications', icon: Bell },
         { title: 'Chat', href: '/chatify', icon: MessageCircle },
         { title: 'Order List', href: '/orders/partner-list', icon: ListOrdered }, // changed link
-        { title: 'Send Feedback', href: '/feedback', icon: MessageCircle },
+        { title: 'Send Feedback', href: '/feedback', icon: FileText },
     ];
     const adminNavItems = [
         { title: 'Manage Users', href: '/admin/users', icon: Bell },
@@ -37,13 +37,23 @@ export function AppSidebar() {
             : []),
         ...(isCustomer
             ? [
-                  { title: 'Send Feedback', href: '/feedback', icon: MessageCircle },
+                  { title: 'Send Feedback', href: '/feedback', icon: FileText },
                   { title: 'Chat', href: '/chatify', icon: MessageCircle },
                   { title: 'Order List', href: '/orders', icon: ListOrdered },
                   { title: 'Notification', href: '/notifications', icon: Bell },
               ]
             : []),
+<<<<<<< HEAD
+        ...(isAdmin
+            ? [
+                  { title: 'Manage Users', href: '/admin/users', icon: Users },
+                  { title: 'Manage Order', href: '/admin/orders', icon: ListOrdered },
+                  { title: 'User Feedback', href: '/admin/feedback', icon: FileText },
+              ]
+            : []),
+=======
         ...(isAdmin ? adminNavItems : []),
+>>>>>>> main
         ...(isPartner ? partnerNavItems : []),
     ];
 
